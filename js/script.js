2d6069d6b96bd7a4890f58aed098a2abe05b11a2
Jarko Mach <jarko.mach@gmail.com>
2024-01-14 17:04:56 +0100

    Remove class active from section class posts

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 "use strict"
 
+const links = document.querySelectorAll('.titles a');
+
 const titleClickHandler = function(event){
     console.log('Link was clicked!', event);
   
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+  
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+  
   
     /* get 'href' attribute from the clicked link */
   
@@ -16,7 +28,6 @@ const titleClickHandler = function(event){
     /* add class 'active' to the correct article */
   }
   
-  const links = document.querySelectorAll('.titles a');
   
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
